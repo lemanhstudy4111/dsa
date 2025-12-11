fdaf7949e137d1b9f3aec08126751e58f8ad2d23
Anh Le <anh.le.nvsu@gmail.com>
2025-12-11 11:12:30 -0500

    ignore pycache, add check balance test

--- tree/check_balance.py
+++ tree/check_balance.py
@@ -1,3 +1,4 @@
+import random
 from binary_tree import TreeNode, BinaryTree
 
 
@@ -8,8 +9,8 @@ def check_balance(bt):
         # leaf
         if not node.left or not node.right:
             return (0, True)
-        left_d = check_balance_helper(node)
-        right_d = check_balance_helper(node)
+        left_d = check_balance_helper(node.left)
+        right_d = check_balance_helper(node.right)
         if not left_d[1] or not right_d[1]:
             return -1, False
         if left_d[0] + 1 > right_d[0] + 2:
@@ -17,3 +18,15 @@ def check_balance(bt):
         return (max(left_d + 1, right_d + 1), True)
 
     return check_balance_helper(bt.root)[1]
+
+
+if __name__ == "__main__":
+    N = 10
+    arr1 = [random.randint(1, 9) for _ in range(N)]
+    print(arr1)
+    bitree = BinaryTree()
+    bitree.list_to_tree_iterative(arr1)
+    print("--Tree--")
+    bitree.print_tree("pre")
+    print()
+    check_balance(bitree)
