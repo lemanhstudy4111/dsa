eedbfa56e35e7511c243b2a95629ec71a7565a75
Anh <anh.le.nvsu@gmail.com>
2025-12-11 21:29:08 -0500

    add test check balance

--- tree/check_balance.py
+++ tree/check_balance.py
@@ -12,21 +12,20 @@ def check_balance(bt):
         left_d = check_balance_helper(node.left)
         right_d = check_balance_helper(node.right)
         if not left_d[1] or not right_d[1]:
-            return -1, False
+            return (-1, False)
         if left_d[0] + 1 > right_d[0] + 2:
-            return -1, False
-        return (max(left_d + 1, right_d + 1), True)
+            return (-1, False)
+        val = max(left_d[0] + 1, right_d[0] + 1)
+        return (val, True)
 
     return check_balance_helper(bt.root)[1]
 
 
-if __name__ == "__main__":
-    N = 10
-    arr1 = [random.randint(1, 9) for _ in range(N)]
-    print(arr1)
-    bitree = BinaryTree()
-    bitree.list_to_tree_iterative(arr1)
-    print("--Tree--")
-    bitree.print_tree("pre")
-    print()
-    check_balance(bitree)
+class TestClass:
+    def test_balance(self):
+        N = 10
+        arr1 = [random.randint(1, 9) for _ in range(N)]
+        print(arr1)
+        bitree = BinaryTree()
+        bitree.list_to_tree_iterative(arr1)
+        assert check_balance(bitree)
